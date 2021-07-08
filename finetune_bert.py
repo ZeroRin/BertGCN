2f5361afe3f3e028a0bf7115eb677dbf442a7bea
zerorin <zerorin>
2021-07-08 10:53:09 +0800

    modified default param

--- finetune_bert.py
+++ finetune_bert.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--max_length', type=int, default=128, help='the input length for bert')
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--nb_epochs', type=int, default=60)
-parser.add_argument('--bert_lr', type=float, default=1e-3)
+parser.add_argument('--bert_lr', type=float, default=1e-4)
 parser.add_argument('--dataset', default='20ng', choices=['20ng', 'R8', 'R52', 'ohsumed', 'mr'])
 parser.add_argument('--bert_init', type=str, default='roberta-base',
                     choices=['roberta-base', 'roberta-large', 'bert-base-uncased', 'bert-large-uncased'])
